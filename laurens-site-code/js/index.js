4a78d7e3d2dfba4ccbd6e4cae311c84f5f4fd6c3
dylan <dylanwozny14@gmail.com>
2021-07-05 18:46:29 -0600

    added js for menu

--- laurens-site-code/js/index.js
+++ laurens-site-code/js/index.js
@@ -11,7 +11,7 @@ function initMap() {
   var marker = new google.maps.Marker({ position: uluru, map: map });
 }
 
-("use strict");
+
 
 window.addEventListener("load", function (event) {
   let menuOpen = false;
@@ -19,50 +19,4 @@ window.addEventListener("load", function (event) {
   const menuLinks = document.querySelectorAll(".mobile-link");
   const menuPanel = document.querySelector("#menu-panel");
 
-  Array.from(menuLinks).forEach((item) => {
-    item.addEventListener("click", function () {
-      menuPanel.removeEventListener("mouseleave", onLeavePanel);
-      closeMobileMenu();
-    });
-  });
-
-  mobileMenuButton.onclick = (e) => {
-    menuOpen ? closeMobileMenu() : openMobileMenu();
-  };
-  function onLeavePanel() {
-    closeMobileMenu();
-  }
-  const openMobileMenu = (e) => {
-    menuPanel.addEventListener("mouseleave", onLeavePanel);
-    TweenLite.to(menuPanel, 0.5, { top: 65, ease: Power1.easeOut });
-    menuOpen = true;
-  };
-
-  const closeMobileMenu = (e) => {
-    menuPanel.removeEventListener("mouseleave", onLeavePanel);
-    TweenLite.to(menuPanel, 0.8, {
-      top: -550,
-      ease: Power1.easeOut,
-    });
-    menuOpen = false;
-  };
 });
-
-const API_KEY = "1234";
-const options = { write: __dirname + "/data" };
-const qpx = require("google-flights-api")(API_KEY, options);
-
-const q = {
-  adultCount: 1,
-  maxPrice: "EUR5000",
-  solutions: 1,
-  origin: "DUB",
-  destination: "GDN",
-  date: "2016-12-14",
-};
-qpx
-  .query(q)
-  .then((data) => {
-    //data looks like: [ { airline: 'SK', price: 'EUR71.10' } ]
-  })
-  .catch(console.error);
--- laurens-site-code/js/menu/mobileme.js
+++ laurens-site-code/js/menu/mobileme.js
@@ -4,19 +4,40 @@ window.onload = function() {
    const mobileMenuButton = document.querySelector('#mobile-menu-button')
    const mobileMenu = document.querySelector("#mobile-menu")
    let isOpen =false;
+
    mobileMenuButton.addEventListener("click", onRequestMenu);
+   console.log(isOpen);
+
+   // close menu
+   function closeMenu(){
+      mobileMenu.classList.toggle("hide-mobile-menu");
+   }
   
-  
+   //   open on click
    function onRequestMenu(e){
-      console.log(click)
-     mobileMenu.classList.add("show-mobile-menu") 
-        isOpen = !isOpen;
-        mobileMenu.classList.toggle("show-mobile-menu");
-        mobileMenu.classList.toggle("hide-mobile-menu")
-   
-      
-     }
-  };
+      console.log("click");
+      mobileMenu.classList.toggle("show-mobile-menu");        
+   }
+
+   // ----Creating logic for closing when anything else is clicked---
+   // onclick >>>>>>
+   // scrolling closes menu
+   if(mobileMenu.classList("show-mobile-menu")){
+      isOpen = true;
+      console.log(isOpen);
+
+   }
+
+   else{
+      isOpen = false;
+      console.log(isOpen);
+
+   }
+
+
+   }
+
+
 
 
  
